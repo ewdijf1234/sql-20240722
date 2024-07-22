52b6f6b47bf164cead72f71e565cbab47b554fbc
ewdijf1234 <jjihye0609@naver.com>
2024-07-22 17:18:19 +0900

    20220722 17:18

--- DML.sql
+++ DML.sql
@@ -41,5 +41,27 @@ UPDATE example_table SET column1 = '선택 수정 데이터' WHERE example_colum
 DELETE FROM example_table WHERE column1 = '수정 데이터';
 
 -- DELETE FROM 테이블명
-DELETE FROM example_table;
+DELETE FROM example_table; # DROP은 테이블 구조를 다 날리는 것과 달리 DELETE 테이블 구조는 남겨두고 안의 레코드를 날림 
 
+CREATE TABLE auto_table (
+	idx INT PRIMARY KEY AUTO_INCREMENT,
+    num INT
+);
+
+-- DROP TABLE : DDL 테이블 구조 전체를 제거
+-- TRUNCATE TABLE : DDL 테이블 구조만 남기고 상태를 초기화
+-- DELETE FROM : DML 테이블 레코드만 제거
+
+-- INSERT INTO SELECT : 삽입 작업시에 조회 결과를 사용하여 삽입
+-- INSERT INTO example_table
+SELECT * FROM example_table WHERE column1 IS NULL;
+
+-- UPDATE SELECT : 수정 작업시 조회 결과를 사용하여 수정 (수정 값에 대하여)
+UPDATE example_table A
+SET column1 = (SELECT column2 FROM example_table WHERE example_column2 = '데이터2');
+
+INSERT INTO auto_table (num) VALUES (0);
+SELECT * FROM auto_table;
+
+DELETE FROM auto_table;
+TRUNCATE TABLE auto_table; # 데이터 구조를 처음으로 되돌린다.
